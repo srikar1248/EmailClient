0d2616cc18b512faafc381a1f58ab75e3893b953
srikar1248 <srikar1248@gmail.com>
2018-08-02 21:52:49 +0530

    Add files via upload

--- EmailWeb/Global.asax.cs
+++ EmailWeb/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SendEmailApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -11,8 +13,7 @@ namespace EmailWeb
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
