4bbe49afe1938c8e5373c63676747581367df5e1
srikar1248 <srikar1248@gmail.com>
2018-08-02 21:53:22 +0530

    Add files via upload

---
+++ EmailWeb/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SendEmailApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/notification/send",
+                defaults: new { id = RouteParameter.Optional, controller = "Notification", action = "send" }
+            );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi1",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
